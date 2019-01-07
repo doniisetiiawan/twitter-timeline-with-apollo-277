4428cc309cf51b662b92d343a1fcdb763f4158e5
Doni Setiawan <donisetiawan.050@gmail.com>
2019-01-07 19:54:38 +0700

    Add tweet model

---
+++ backend/src/model/Tweet.js
@@ -0,0 +1,49 @@
+import mongoose from 'mongoose';
+
+mongoose.connect('mongodb://localhost/twitter');
+
+const Schema = mongoose.Schema;
+
+const tweetSchema = new Schema({
+  tweet: String,
+  author: String,
+  createdAt: Date,
+});
+
+const TweetModel = mongoose.model('Tweet', tweetSchema);
+
+export default {
+  getTweets: () => TweetModel.find().sort({ _id: -1 }),
+  getTweet: _id => TweetModel.findOne({ _id }),
+  createTweet: args => TweetModel(args).save(),
+  deleteTweet: (args) => {
+    const { _id } = args;
+
+    TweetModel.remove({ _id }, (error) => {
+      if (error) {
+        console.log('Error Removing:', error);
+      }
+    });
+
+    return args;
+  },
+  updateTweet: (args) => {
+    const { _id, tweet } = args;
+
+    TweetModel.update({ _id }, {
+      $set: {
+        tweet,
+      },
+    },
+    { upsert: true }, (error) => {
+      if (error) {
+        console.log('Error Updating:', error);
+      }
+    });
+
+    args.author = 'codejobs';
+    args.createdAt = new Date();
+
+    return args;
+  },
+};
