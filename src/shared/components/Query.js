1383a73286d418b40d55ec543576e119b4f48225
Doni Setiawan <donisetiawan.050@gmail.com>
2019-01-08 10:48:47 +0700

    Add graphql query and mutation helper

---
+++ src/shared/components/Query.js
@@ -0,0 +1,32 @@
+/* eslint-disable react/prop-types,
+react/prefer-stateless-function,
+no-shadow,react/jsx-filename-extension */
+import React, { Component } from 'react';
+import { Query as ApolloQuery } from 'react-apollo';
+
+class Query extends Component {
+  render() {
+    const {
+      query,
+      render: Component,
+    } = this.props;
+
+    return (
+      <ApolloQuery query={query}>
+        {({ loading, error, data }) => {
+          if (loading) {
+            return <p>Loading...</p>;
+          }
+
+          if (error) {
+            return <p>Query Error: {error}</p>;
+          }
+
+          return <Component data={data || false} />;
+        }}
+      </ApolloQuery>
+    );
+  }
+}
+
+export default Query;
