9ed4da7a5fe2b42b420d09f062d2faac066e96b6
Doni Setiawan <donisetiawan.050@gmail.com>
2019-01-08 10:46:57 +0700

    Refactor index

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
+/* eslint-disable react/jsx-filename-extension */
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import { render } from 'react-dom';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const client = new ApolloClient({
+  uri: 'http://localhost:5000/graphql',
+});
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const AppContainer = () => (
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+);
+
+const root = document.getElementById('root');
+
+render(<AppContainer />, root);
